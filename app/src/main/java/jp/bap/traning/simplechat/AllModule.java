e29481f5351938eb401da7bb7c3e22ed280df833
dungpv <dungpv@bap.jp>
2018-06-11 08:26:01 +0700

    update for use realmObject

---
+++ app/src/main/java/jp/bap/traning/simplechat/AllModule.java
@@ -0,0 +1,11 @@
+package jp.bap.traning.simplechat;
+
+import io.realm.annotations.RealmModule;
+
+/**
+ * Created by dungpv on 6/11/18.
+ */
+
+@RealmModule(library = true, allClasses = true)
+public class AllModule {
+}
--- app/src/main/java/jp/bap/traning/simplechat/BaseApp.java
+++ app/src/main/java/jp/bap/traning/simplechat/BaseApp.java
@@ -2,9 +2,14 @@ package jp.bap.traning.simplechat;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
 import org.androidannotations.annotations.EApplication;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.rx.RealmObservableFactory;
 
 /**
  * Created by dungpv on 6/7/18.
@@ -25,5 +30,24 @@ public class BaseApp extends MultiDexApplication {
         super.onCreate();
         sInstance=this;
         Realm.init(this);
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .modules(Realm.getDefaultModule(), new AllModule())
+                .name(Realm.DEFAULT_REALM_NAME)
+                .rxFactory(new RealmObservableFactory())
+                .schemaVersion(0)
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+
+        if (BuildConfig.DEBUG) {
+            /**
+             * set up for view realm db in #chrome://inspect/
+             */
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                            .build());
+        }
     }
 }
