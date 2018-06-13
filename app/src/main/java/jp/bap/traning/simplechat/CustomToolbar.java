0b20aca4e2bb793ca613c98b7fd08e4897ef3ce7
ADMIN\Admin <nguyenthiynhi.4n@gmal.com>
2018-06-13 15:38:04 +0700

    commit

--- app/src/main/java/jp/bap/traning/simplechat/CustomToolbar.java
+++ app/src/main/java/jp/bap/traning/simplechat/CustomToolbar.java
@@ -6,18 +6,20 @@ import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RelativeLayout;
+
+import org.androidannotations.annotations.EView;
+import org.androidannotations.annotations.EViewGroup;
 
 import jp.bap.traning.simplechat.R;
 
 /**
  * Created by Admin on 6/13/2018.
  */
-
+@EViewGroup(R.layout.layout_custom_toolbar)
 public class CustomToolbar extends Toolbar {
+
     public CustomToolbar(Context context) {
         super(context);
-//        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        View view = inflater.inflate(R.layout.layout_custom_toolbar,this,true);
-
     }
 }
