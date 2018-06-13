b8e1c2e9e525d34fd66189f8cdc64f004308755b
dungpv <dungpv@bap.jp>
2018-06-13 11:44:15 +0700

    update code

---
+++ app/src/main/java/jp/bap/traning/simplechat/Common.java
@@ -0,0 +1,21 @@
+package jp.bap.traning.simplechat;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by dungpv on 6/13/18.
+ */
+
+public class Common {
+    private static final String URL_SERVER = "http://172.16.1.77:3000";
+
+    public static void connectToServerSocket(Context context, String host, int token) {
+        if (ChatService.getChat() == null) {
+            Intent i = new Intent(context, ChatService.class);
+            i.putExtra("host", host);
+            i.putExtra("token", token);
+            context.startService(i);
+        }
+    }
+}
--- app/src/main/java/jp/bap/traning/simplechat/MainActivity.java
+++ app/src/main/java/jp/bap/traning/simplechat/MainActivity.java
@@ -1,11 +1,8 @@
 package jp.bap.traning.simplechat;
 
-import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.Log;
-import android.view.View;
 
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
@@ -16,20 +13,7 @@ public class MainActivity extends BaseActivity {
     AppCompatEditText mEditText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
     public void afterView() {
 
-    }
-
-    @Click(R.id.mBtnSend)
-    void onClick(View view) {
-        if (ChatService.getChat() != null) {
-            ChatService.getChat().sendMessage(mEditText.getText().toString());
-        }
     }
 }
